6627bd20c360e9f1b9487fbae574ce98f75bad66
walid <guiratwalid98@gmail.com>
2025-03-17 13:08:06 +0100

    [ADD] creating authors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
@@ -9,7 +10,9 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
   const params = {search: query || null};
+
   const {data:posts} = await sanityFetch({query: STARTUPS_QUERY, params});
 
   return (
